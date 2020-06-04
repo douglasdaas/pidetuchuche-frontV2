266736a923c5be86aa87ff18d80eee38567b088b
douglasdaas <douglasdaas@gmail.com>
2020-06-04 16:02:41 -0400

    Por favor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 // Componentes
@@ -13,15 +13,19 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProductoComponent } from './components/producto/producto.component';
 import { SliderPrincipalComponent } from './components/sliderPrincipal/sliderPrincipal.component';
+import {HeaderComponent} from './components/header/header.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     HomeComponent,
     LoginComponent,
-    FooterComponent, 
+    FooterComponent,
     ProductoComponent,
-    SliderPrincipalComponent
+    SliderPrincipalComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
