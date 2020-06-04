3cc4f4d187d907799386f62f833fe22252d9a3c2
douglasdaas <douglasdaas@gmail.com>
2020-06-04 19:43:50 -0400

    gzip

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const compression = require('compression')
 const path = require('path');
 
 const app = express();
 
 app.use(express.static(__dirname+'/../dist/pidetuchuche-Angular'));
-app.use(compression())
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('dist/pidetuchuche-Angular'));
