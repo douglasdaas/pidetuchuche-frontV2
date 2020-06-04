d295173d614f0ae46e7f9adb74bd0f9853fc1a7e
douglasdaas <douglasdaas@gmail.com>
2020-06-04 18:26:40 -0400

    Heroku listo

--- src/server.js
+++ src/server.js
@@ -17,8 +17,14 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname+'/../dist/pidetuchuche-Angular'));
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/../dist/pidetuchuche-Angular/index.html'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('dist/pidetuchuche-Angular'));
+}
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, '../dist/pidetuchuche-Angular', 'index.html'));
+  // response.sendFile( '/dist/pidetuchuche-Angular', 'index.html');
 });
 
 app.listen(process.env.PORT || 8080);
