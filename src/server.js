f2b5eb03301e36bddc825a83a1fe18b1b9803c57
douglasdaas <douglasdaas@gmail.com>
2020-06-04 16:24:49 -0400

    ver

--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
+// const express = require('express');
+// const path = require('path');
+//
+// const app = express();
+//
+// app.use(express.static('.dist/pidetuchuche-Angular'));
+// app.get('/*', function(req, res) {
+//   res.sendFile('index.html', {root: 'dist/pidetuchuche-Angular'}
+// );
+// });
+//
+// app.listen(process.env.PORT || 8080);
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/pidetuchuche-angular'));
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/pidetuchuche-angular'}
-);
+app.use(express.static(__dirname+'/../dist/pidetuchuche-Angular'));
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/../dist/pidetuchuche-Angular/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
