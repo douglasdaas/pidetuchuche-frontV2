0b0b2cda6a7d672f54c909fae3b2d0fd045cdb8d
douglasdaas <douglasdaas@gmail.com>
2020-06-05 12:47:13 -0400

    fixes 3

--- src/app/services/categoria/categoria.service.ts
+++ src/app/services/categoria/categoria.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { GLOBAL } from '../../../environments/environment';
+import { GLOBAL} from '../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
