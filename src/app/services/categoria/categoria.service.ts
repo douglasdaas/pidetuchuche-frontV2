f2a18889fcdbdb6f344836601ca2c95a75d7d1b1
OscarM04 <oscarmarquez2218@gmail.com>
2020-06-05 12:05:16 -0400

    Reduccion de peso de logo

--- src/app/services/categoria/categoria.service.ts
+++ src/app/services/categoria/categoria.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { GLOBAL } from '../global';
-import { Categoria } from '../../models/categoria';
+import { GLOBAL } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
--- src/app/services/global.ts
+++
@@ -1,5 +0,0 @@
-export let GLOBAL = {
-    url: 'https://pidetuchuche-backend.herokuapp.com',
-    urlLocal: 'http://127.0.0.1:3334',
-    urlTest : 'https://pidetuchuche-test-backend.herokuapp.com'
-};
